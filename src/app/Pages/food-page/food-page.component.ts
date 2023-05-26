78e51c6c43fb7e450a8bee96fe3e426104d42701
Eimer Perea <ing6soporte@btw.com.co>
2023-05-25 23:23:22 -0500

    MiMenu maqueta

---
+++ src/app/Pages/food-page/food-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/Services/food/food.service';
+import { Food } from 'src/app/Shared/Models/Food';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/Services/cart/cart.service';
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrls: ['./food-page.component.css']
+})
+export class FoodPageComponent implements OnInit {
+
+  food!: Food;
+  constructor(private activatedRoute:ActivatedRoute,
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router: Router) { 
+    activatedRoute.params.subscribe((params) => {
+      if(params["id"])
+      this.food = foodService.getFoodById(params["id"]);
+    })
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { FoodPageComponent } from './Pages/food-page/food-page.component';
+import { CartPageComponent } from './Pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FoodPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
